600e1aad0c47b0be97da0de64b66a9ad1a720791
MrEdzilla <43966574+MrEdzilla@users.noreply.github.com>
2025-07-22 12:19:39 +0100

    Adicionar idade

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from datetime import date
+
 print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
+
+idade: int = input("Quantos anos tem? ")
+
 print(f"Olá, {nome}!")
+
+print(f"O/A {nome} tem {idade} anos de idade!")
