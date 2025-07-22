e367b797dcdcb24d494c57a7bade3dc1f1295efb
MrEdzilla <43966574+MrEdzilla@users.noreply.github.com>
2025-07-22 12:41:27 +0100

    Resumo da informação adicionado

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ ano_nascimento = ano_atual - idade
 
 cidade: str = print("Em que cidade é que mora? ")
 
-profissao = input("Qual é a sua profissão de sonho? ")
+profissao = input("Qual é a sua profissão ")
 
 print(f"O/A {nome} e vive na localidade de {cidade}!")
 print(f"Nasceu no ano de {ano_nascimento} e, como tal, tem {idade} anos de idade!")
-print(f"A profissão de sonho de {nome} é ser {profissao}!")
+print(f"A profissão de {nome} é {profissao}!")
+
+print(f"\nResumo:")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Cidade: {cidade}")
+print(f"Profissão: {profissao}")
