1398de0a3456c99780b444c80706b706e2c64fd8
MrEdzilla <43966574+MrEdzilla@users.noreply.github.com>
2025-07-22 12:33:41 +0100

    Adicionar profissão

--- main.py
+++ main.py
@@ -12,4 +12,7 @@ idade: int = input("Quantos anos tem? ")
 
 cidade: str = print("Em que cidade é que mora? ")
 
+profissao = input("Qual é a sua profissão de sonho? ")
+
 print(f"O/A {nome} tem {idade} anos de idade, e vive na localidade de {cidade}!")
+print(f"A profissão de sonho de {nome} é ser {profissao}!")
